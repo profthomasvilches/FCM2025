b00b849a9c6a31b4c4cb86f51ef4580cb2af4dfa
thomasvilches <thomvilches@gmail.com>
2025-10-30 10:25:18 -0300

    Aula Duck DB

--- Praticas_R/datasus.R
+++ Praticas_R/datasus.R
@@ -1,6 +1,5 @@
 
 
-#read.dbc::read.dbc()
 
 
 library(tidyverse)
@@ -95,5 +94,91 @@ ggplot(df_final)+
 
 ggplot(df_final)+
   geom_boxplot(aes(x = factor(Ano), y = incidencia))
+
+
+#### Lendo dados DBC
+
+
+library(tidyverse)
+
+
+#read.dbc::read.dbc()
+
+
+dados <- read.dbc::read.dbc("Dados/DENGBR24.dbc")
+
+nrow(dados)
+
+names(dados)
+
+glimpse(dados)
+
+dados_sp <- dados %>% 
+  filter(grepl("^35", ID_MN_RESI))
+
+nrow(dados_sp)
+
+rm(dados)
+
+glimpse(dados_sp)
+
+write.csv(dados_sp, "dados_dengue_sp.csv")
+
+names(dados_sp)
+
+dados_sp %>% 
+  select(CLASSI_FIN) %>% unique
+
+
+
+############# trabalhar com conjunto de dados muito grandes
+
+library(tidyverse)
+library("duckdb")
+
+con <- dbConnect(duckdb::duckdb(), dbdir = "Dados/meu_banco.duckdb")
+# com os dados em um CSV
+dbExecute(con, "CREATE TABLE dados AS SELECT * FROM read_csv_auto('dados_dengue_sp.csv', encoding='latin-1', sep = ';')")
+
+# com os dados no R
+dbWriteTable(con, "dados", dados_sp, overwrite = TRUE)
+
+# Consultas SQL diretas (sem carregar na RAM!)
+dbGetQuery(con, "DESCRIBE dados")
+
+sem_pri <- dbGetQuery(con, 'SELECT SEM_PRI FROM dados WHERE CLASSI_FIN IN (10, 11, 12)')
+
+
+df_sem_prim <- data.frame(sem_pri)
+
+df_sem_prim %>% 
+  group_by(SEM_PRI) %>% 
+  summarise(
+    n = n()
+  ) %>% 
+  ungroup() %>% 
+  ggplot()+
+  geom_col(aes(x = SEM_PRI, y = n))+
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1.0, vjust = 0.5)
+  )
+
+
+
+################## Dados de mortalidade
+
+
+dados_simdo <- read.dbc::read.dbc("Dados/DATASUS/DOAC2015.dbc")
+glimpse(dados_simdo)
+
+
+# rastreando neoplasias CID
+
+dados_cancer <- dados_simdo %>% 
+  filter(
+    grepl("^C|^D[1234]", CAUSABAS)
+  )
+
+nrow(dados_cancer)
 
 
